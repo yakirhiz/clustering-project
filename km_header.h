835f512e67e1c9fb0dc7b95b32f788da0977ba1e
Yakir Hizkiyahu <yakir_hizkiyahu_tp@bmc.com>
2024-03-07 09:45:25 +0200

    Fix declaration

--- km_header.h
+++ km_header.h
@@ -5,13 +5,12 @@
 #define MALLOC_CHECK(x) if(x==NULL){puts("\nMalloc Fail\n"); exit(0);}
 
 double *kmeans(int K, int N, int d, int MAX_ITER, double *N_observations, double *cents);
-void print_centroids(int K, int d, double *cents);
+void print_centroids(double *cents, int K, int d);
 double squared_euclidean_distance(double vec1[], double vec2[], int size);
 int findClosestCent(int d, int K, double *cents, double *obs);
 double *calcNewCentroids(int K, int N, int d, double *cents, double *N_observation);
 int centsChanged(int K, int d, double *cents, double *newCents);
 double *readFile(int d, int N, char *path);
-void printMat(const double *mat, int n, int d);
 double *readStdin(int N, int d);
 double *initializeCentroids(int K, int d, double *N_observations);
 
