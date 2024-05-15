ae5d6ce6d3059cd8f405de802bb5d39e11b7690d
Yakir Hizkiyahu <yakir_hizkiyahu_tp@bmc.com>
2024-05-15 18:17:11 +0300

    Rename genRandPoints() to generate_random_points(); change usage of  random.randint() to np.random.randint()

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import kmeans_pp as km
 import SpecClust as sc
 import numpy as np
-import random
 from sklearn.datasets import make_blobs
 
 import plot_module as plotmod
@@ -15,7 +14,7 @@ import text_module as textmod
 # input:    K = the number of clusters
 #           N = the number of points
 #           rand (bool) = if true draw K and N from (MAX_CAPACITY/2, MAX_CAPACITY)
-# output:   an ndarray Nxd of N points of dimention d
+# output:   an ndarray Nxd of N points of dimension d
 def main(K, N, rand):
     max_cap_k = 20
     max_cap_n = 1000
@@ -24,15 +23,15 @@ def main(K, N, rand):
     print(f"Max capacity: k={max_cap_k}, n={max_cap_n}.")
 
     if rand:
-        N = random.randint(max_cap_n // 2, max_cap_n) 
-        K = random.randint(max_cap_k // 2, min(max_cap_k, N)) # K cannot be larger than N
+        N = np.random.randint(max_cap_n // 2, max_cap_n)
+        K = np.random.randint(max_cap_k // 2, min(max_cap_k, N)) # K cannot be larger than N
         
     K_used_in_blobs = K
-    
-    # create points to be clustered
-    points, sklearn_clusters = genRandPoints(N, K)
 
-    d = points.shape[1] # the dimention of the points
+    # create points to be clustered
+    points, sklearn_clusters = generate_random_points(N, K)
+
+    d = points.shape[1] # the dimension of the points
 
     if rand:
         # cluster with Spectral Clustering
@@ -55,11 +54,11 @@ def main(K, N, rand):
     # Textual Output
     textmod.output_results(K, N, d, points, sklearn_clusters, sc_clusterIndecies, km_clusterIndecies)
 
-# genRandPoints - creates the random points to be sent to the clustering algorithms.
+# generate_random_points - creates the random points to be sent to the clustering algorithms.
 #
 # input:    N = number of points
 #           K = the number of blobs
-# output:   an ndarray (Nxd) of N points of dimention d
-def genRandPoints(N, K):
-    dim = random.randint(2, 3)  # The dimention is chosen randomly between 2 and 3.
+# output:   an ndarray (Nxd) of N points of dimension d
+def generate_random_points(N, K):
+    dim = np.random.randint(2, 4)  # The dimension is chosen randomly between 2 and 3.
     return make_blobs(N, dim, centers=K)
